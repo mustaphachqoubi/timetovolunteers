0b32be7ff2bec894387281cb94e4f0cc7a6fc5ff
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2022-12-20 21:59:30 +0100

    working on the Campaign componenet

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import Campaign_container from "./Components/Campaign-container/Campaign_container";
+function App() {
+  return (
+    <div className="App">
+      <Campaign_container />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/Campaign-container/Campaign_container.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Campaign from '../Campaign/Campaign'
+import './style.css'
+
+const Campaign_container = () => {
+  return (
+    <div className='Campaign_container'>
+        <Campaign />
+    </div>
+  )
+}
+
+export default Campaign_container
