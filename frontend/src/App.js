5dc6782f52ad542c0ecc08e3f9a56e4eb5e74b59
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2022-12-23 22:18:51 +0100

    adding responsiveness

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,17 @@
 import Campaign_container from "./Components/Campaign-container/Campaign_container";
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    // Using fetch to fetch the api from 
+    // flask server it will be redirected to proxy
+    fetch("/data").then((res) =>
+        res.json().then((data) => {
+            // Setting a data from api
+            console.log(data)
+        })
+    );
+}, []);
   return (
     <div className="App">
       <Campaign_container />
